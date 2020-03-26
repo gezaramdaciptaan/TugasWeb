95e9eb54191e31b85a817ec1f65c54f96043cc54
gezaramdaciptaan <Gezaramdaciptaan@gmail.com>
2020-03-27 01:36:36 +0700

    Updates

---
+++ precache-manifest.c8dec8a849c251ca2db93698de16e3a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1773e3bdbd9c953dd02abeb92434fa4",
+    "url": "/TugasWeb/index.html"
+  },
+  {
+    "revision": "401c0d95a595edb4175f",
+    "url": "/TugasWeb/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "f3a7588dae41a90e9807",
+    "url": "/TugasWeb/static/css/main.d1c6881f.chunk.css"
+  },
+  {
+    "revision": "401c0d95a595edb4175f",
+    "url": "/TugasWeb/static/js/2.8113d7c8.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/TugasWeb/static/js/2.8113d7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a7588dae41a90e9807",
+    "url": "/TugasWeb/static/js/main.5afdc64f.chunk.js"
+  },
+  {
+    "revision": "1a561fd05b5903e0a049",
+    "url": "/TugasWeb/static/js/runtime-main.36d456d4.js"
+  },
+  {
+    "revision": "d1ceef0382c613681abafc5b692b1890",
+    "url": "/TugasWeb/static/media/picture.d1ceef03.svg"
+  }
+]);
